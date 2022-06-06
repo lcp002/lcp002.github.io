4db336f8d4bc842a4c3c1f947960b463e101877a
lcp002 <105829909+lcp002@users.noreply.github.com>
2022-06-05 23:48:01 -0500

    Update pritchardEngine.js
    Introduced switch statements within attack function to allow for special moves (such as healing)

--- pritchardEngine.js
+++ pritchardEngine.js
@@ -1,11 +1,11 @@
 
 let characterList = [
     [
-        "Lando", "lando.png", 100, [
-            ["Tackle", 1, 5, 0, 100,], //Move Name, Minimum Damage, Maximum Damage, Lower Miss Value, Higher Miss Value
-            ["Kick", 5, 10, 0, 100,],
-            ["Bite", 10, 50, 5, 100,],
-            ["Rage", 1, 100, 10, 100,],
+        "Lando", "lando.png", 75, [ //Increased health
+            ["Tackle", 1, 30, 50, 100,], //Move Name, Minimum Damage, Maximum Damage, Lower Miss Value, Higher Miss Value
+            ["Holy Blessing", 75, 75, 50, 100,],
+            ["Holy Blessing", 75, 75, 50, 100,], //Nerfed bite
+            ["Holy Blessing", 75, 75, 50, 100,], //Example of a Healing Move
         ],
     ],
     [
@@ -46,6 +46,7 @@ $(document).ready(function() {
     spawnP1();
     spawnP2();
     hideGameInfoBox();
+    createLadder();
 });
 
 class Character {
@@ -61,6 +62,15 @@ class Character {
 var player1=null
 var player2=null
 var gameOver=false
+
+function createLadder() { //Create an array with a ladder of characters to kill
+    let ladder = [];
+    for (i = 0; i < 3; i++) {
+        let randInt = Math.floor(Math.random() * characterList.length);
+        ladder.push(characterList[randInt]);
+    }
+    console.log(ladder);
+}
 
 function spawnP1() {
     let p = characterList[Math.floor(Math.random() * characterList.length)];
@@ -92,7 +102,7 @@ function checkIfSame() {
     };
 };
 
-function attack(attacker, reciever, move) {
+function attack(attacker, reciever, move) { //why the hell are players attacking themselves when facing lando??? It happens when his health goes over his max health
     showGameInfoBox();
     if (attacker.hp <= 0) {
         attacker.hp = 0;
@@ -101,6 +111,15 @@ function attack(attacker, reciever, move) {
         return;
     }
 
+    if (attacker.hp > attacker.fullhp) {
+        attacker.hp = attacker.fullhp
+        if (attacker = player1) {
+            $('#playerHealth').val(attacker.hp);
+        } else {
+            $('#computerHealth').val(attacker.hp);
+        };
+    };
+
     animateMove(attacker, reciever, move);
 
     let damage = Math.floor(Math.random() * move[2]) + move[1];
@@ -108,21 +127,48 @@ function attack(attacker, reciever, move) {
         damage = Math.floor(Math.random() * move[2]) + move[1];
     };
     let missChance = Math.floor(Math.random() * move[4]) + 1;
-    if (missChance <= move[3]) {
-        damage = 0;
-        document.getElementById('gameInfoBox').innerHTML = attacker.name + " did " + move[0].toLowerCase() + "! It missed!";
-    } else {
-        document.getElementById('gameInfoBox').innerHTML = attacker.name + " did " + move[0].toLowerCase() + "! It did " + damage + " damage!";
+
+    switch (move[0]) {
+        case "Holy Blessing":
+            if (missChance <= move[3]) {
+                damage = 0;
+                document.getElementById('gameInfoBox').innerHTML = attacker.name + " did " + move[0].toLowerCase() + "! It missed!";
+            } else {
+                document.getElementById('gameInfoBox').innerHTML = attacker.name + " healed for " + damage + " health!"
+                attacker.hp += damage;
+                $('#playerHealth').val(player1.hp);
+                $('#computerHealth').val(player2.hp);
+            };
+            break;
+
+        default:
+            if (missChance <= move[3]) {
+                damage = 0;
+                document.getElementById('gameInfoBox').innerHTML = attacker.name + " did " + move[0].toLowerCase() + "! It missed!";
+            } else {
+                document.getElementById('gameInfoBox').innerHTML = attacker.name + " did " + move[0].toLowerCase() + "! It did " + damage + " damage!";
+                reciever.hp -= damage;
+                $('#playerHealth').val(player1.hp);
+                $('#computerHealth').val(player2.hp);
+            };
+            break;
     };
-    reciever.hp -= damage;
-    $('#playerHealth').val(player1.hp);
-    $('#computerHealth').val(player2.hp);
+    
+    if (attacker.hp > attacker.fullhp) {
+        attacker.hp = attacker.fullhp
+        if (attacker = player1) {
+            $('#playerHealth').val(attacker.hp);
+        } else {
+            $('#computerHealth').val(attacker.hp);
+        };
+    };
+
     if (reciever.hp <= 0) {
         reciever.hp = 0;
         document.getElementById('gameInfoBox').innerHTML="Game Over! " + reciever.name + " fainted!";
         gameOver=true;
         return;
-    }
+    };
 };
 
 function hideButtons() {
@@ -280,6 +326,16 @@ function animateMove(attacker, reciever, move) {
             $(attackSide).animate({left: "0%"});
             $(receiveSide).animate({height: '275px', width: '275px', top: '25px'});
             $(receiveSide).animate({height: '300px', width: '300px', top: '0px'});
+            break;
+
+        case 'Holy Blessing':
+            if (attacker.name == player1.name) {
+                attackSide = '#playerSprite'; 
+            } else {
+                attackSide = '#computerSprite'; 
+            };
+            $(attackSide).animate({height: '+=30px', width: '+=30px', top: '-=30px'});
+            $(attackSide).animate({height: '-=30px', width: '-=30px', top: '+=30px'});
             break;
 
         };
